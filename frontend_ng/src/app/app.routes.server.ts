9f6ff474af463fa871dc8d3b329a5d4c668df20c
MustafaTheEngineer <mustafaesat01@gmail.com>
2025-10-24 19:47:47 +0300

    CORS to all

--- frontend_ng/src/app/app.routes.server.ts
+++ frontend_ng/src/app/app.routes.server.ts
@@ -1,8 +1,26 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
+export const routesIDs: string[] = ['manage-id', 'item-id', ];
+
 export const serverRoutes: ServerRoute[] = [
   {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'home/item/manage/:id',
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+      const ids = routesIDs;
+      return ids.map((id) => ({ id }));
+    },
+  },
+  {
+    path: 'home/item/:id',
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+      const ids = routesIDs;
+      return ids.map((id) => ({ id }));
+    },
+  },
 ];
